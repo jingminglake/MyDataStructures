fa345f698ed69e72e82516ee001ebad19bb397ed
jingminglake <yaxionh@97.236.21.198.tigernet.wifi.dyn.clemson.edu>
2018-06-01 22:49:22 -0400

    my bubble sort

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 
 void bubblesort(int a[], int size) {
-  bool needSwap = true;
-  for (int i = 0; i < size - 1; i++) {
-    needSwap = false;
-    for (int j = 0; j < size - i - 1; j++) {
-      if (a[j] > a[j + 1]) {
-	int temp = a[j + 1];
-	a[j + 1] = a[j];
-	a[j] = temp;
-	needSwap = true;
-      }
-    } 
-  }
-  if (!needSwap) {
-    return;
-  }
+    bool needSwap = true;
+    for (int i = 0; i < size - 1; i++) { // loop size - 1 time, each time a num will occur in its eventually position
+        needSwap = false;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+                needSwap = true;
+            }
+        } 
+    }
+    if (!needSwap) {
+        return;
+    }
 }
 
 int main()
 {
-  int a[10];
-  for (int i = 0; i < 10; i++) {
-    a[i] = rand() % 100;
-    cout << a[i] << " ";
-  }
-  cout << endl;
-  bubblesort(a, 10);
-  for (int i = 0; i < 10; i++)
-    cout << a[i] << " ";
-  cout << endl;
-  return 0;
+    int a[10];
+    for (int i = 0; i < 10; i++) {
+        a[i] = rand() % 100;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    bubblesort(a, 10);
+    for (int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+    cout << endl;
+    return 0;
 }
